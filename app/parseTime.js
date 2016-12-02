11b142dca2ce523fbc6365d05e731ee827089b36
Christian Schmidt <chris.kueps@gmail.com>
2016-12-02 16:13:28 +0000

    configurated parseTime function

--- app/parseTime.js
+++ app/parseTime.js
@@ -2,10 +2,20 @@ const parseTime = function (input) {
     const time = isNaN(+input) ? input : (input * 1000);
     const date = new Date(time);
     const dateOptions = {year: 'numeric', month: 'long', day: 'numeric'};
-    const json = {
-        unix: date.getTime() / 1000,
-        natural: date.toLocaleDateString("en-US", dateOptions)
+    
+    const output = {
+        unix: null,
+        natural: null,
     };
-    return JSON.stringify(json);
+    
+    if (isNaN(date.getTime())) {
+        return output;
+    }
+    
+    output.unix = date.getTime() / 1000;
+    output.natural = date.toLocaleDateString("en-US", dateOptions);
+
+    return output;
 };
+
 module.exports = parseTime;
